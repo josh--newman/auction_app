501f804422b462ad8117a5a9ec121f4b5234a646
Josh Newman <josh.newman4390@gmail.com>
2014-01-23 14:19:03 +1000

    Added some styling to the table elements and made the main logo better

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def print_jbay
+    raw("<span class='c1'>J</span>
+         <span class='c2'>B</span>
+         <span class='c3'>a</span>
+         <span class='c4'>y</span>")
+  end
 end
