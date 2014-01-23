c6bb14a2347534ebed5e343935cde5e38a353f8f
Josh Newman <josh.newman4390@gmail.com>
2014-01-23 14:28:12 +1000

    Changed colour scheme and made an application helper to print JBay logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,6 @@
 module ApplicationHelper
 
   def print_jbay
-    raw("<span class='c1'>J</span>
-         <span class='c2'>B</span>
-         <span class='c3'>a</span>
-         <span class='c4'>y</span>")
+    raw("<span class='c1'>J</span><span class='c2'>B</span><span class='c3'>a</span><span class='c4'>y</span>")
   end
 end
