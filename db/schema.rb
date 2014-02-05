0cddeb6ff124102d3ebba0d2724e6c0feae5e51f
Josh Newman <josh.newman4390@gmail.com>
2014-02-05 19:26:51 +1000

    Started A2 documentation and fixed schema

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,17 @@ ActiveRecord::Schema.define(version: 20140203102540) do
 
   add_index "items", ["user_id"], name: "index_items_on_user_id"
 
-# Could not dump table "users" because of following NoMethodError
-#   undefined method `[]' for nil:NilClass
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "password_digest"
+    t.string   "remember_token"
+    t.boolean  "admin",           default: false, null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
